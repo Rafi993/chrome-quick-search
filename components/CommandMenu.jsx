cc97a1062356788e25cb5f4365238b51deb25ef6
rafi993 <raf993@protonmail.com>
2022-08-29 17:21:35 +0530

    Moving command to seperate menu

--- components/CommandMenu.jsx
+++ components/CommandMenu.jsx
@@ -1,6 +1,7 @@
 import { Command } from 'cmdk';
 import FocusLock from 'react-focus-lock';
 import styled from 'styled-components';
+import { commands } from './commands';
 
 const StyledCommandMenu = styled.div`
   position: fixed;
@@ -9,6 +10,7 @@ const StyledCommandMenu = styled.div`
   left: 0;
   right: 0;
   top: 0;
+  z-index: 999999;
 `;
 
 export const CommandMenu = () => {
@@ -21,21 +23,9 @@ export const CommandMenu = () => {
             <Command.Empty>No results found.</Command.Empty>
 
             <Command.Group heading="Commands">
-              <Command.Item>Close Tab</Command.Item>
-              <Command.Item>Duplicate Tab</Command.Item>
-              <Command.Item>Pin Tab</Command.Item>
-
-              <Command.Item>New Window</Command.Item>
-              <Command.Item>New Private Window</Command.Item>
-              <Command.Item>Close Window</Command.Item>
-              <Command.Item>Fullscreen mode</Command.Item>
-              <Command.Item>Settings</Command.Item>
-              <Command.Item>Extensions</Command.Item>
-              <Command.Item>History</Command.Item>
-              <Command.Item>Bookmark manager</Command.Item>
-
-              <Command.Item>Delete Browsing Data</Command.Item>
-              <Command.Item>Delete Browsing Data</Command.Item>
+              {commands.map((command) => (
+                <Command.Item key={command.key}>{command.label}</Command.Item>
+              ))}
             </Command.Group>
           </Command.List>
         </Command>
---
+++ components/commands.js
@@ -0,0 +1,35 @@
+export const commands = [
+  {
+    label: 'Close Tab',
+    key: 'close_tab',
+  },
+  {
+    label: 'Duplicate Tab',
+    key: 'duplicate_tab',
+  },
+  {
+    label: 'Pin Tab',
+    key: 'pin_tab',
+  },
+  {
+    label: 'New Window',
+    key: 'new_window',
+  },
+  {
+    label: 'New Private Window',
+    key: 'new_private_window',
+  },
+  {
+    label: 'Close Window',
+    key: 'close_window',
+  },
+  {
+    label: 'Fullscreen mode',
+    key: 'full_screen',
+  },
+  { label: 'Settings', key: 'settings' },
+  { label: 'Extensions', key: 'extensions' },
+  { label: 'History', key: 'history' },
+  { label: 'Bookmark manager', key: 'bookmark_manager' },
+  { label: 'Delete Browsing Data', key: 'delete_data' },
+];
