ff321093db4d9d32544e8712fd34931e30cf434c
rafi993 <raf993@protonmail.com>
2022-08-31 12:26:27 +0530

    Adding quick search commands

--- background.js
+++ background.js
@@ -10,16 +10,55 @@ chrome.action.onClicked.addListener((tab) => {
   );
 
   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log('request', request);
+    const tabId = sender.tab.id;
+
+    console.log('sender', sender);
+    if (!sender.tab.active) return;
+
     switch (request.command) {
       case 'close_tab':
-        chrome.tabs.remove(sender.tab.id, () => {});
+        chrome.tabs.remove(tabId, () => {});
         return;
       case 'new_tab':
         chrome.tabs.create({
           url: 'chrome://new-tab-page',
         });
         return;
+      case 'duplicate_tab':
+        chrome.tabs.create({
+          url: sender.tab.url,
+        });
+        return;
+      case 'pin_tab':
+        return;
+      case 'new_window':
+        chrome.windows.create({ focused: true });
+        return;
+      case 'new_private_window':
+        chrome.windows.create({ focused: true, incognito: true });
+        return;
+      case 'close_window':
+        chrome.windows.remove(sender.tab.windowId);
+        return;
+      case 'full_screen':
+        chrome.windows.update(sender.tab.windowId, { state: 'fullscreen' });
+        return;
+      case 'settings':
+        chrome.tabs.create({
+          url: 'chrome://settings',
+        });
+      case 'extensions':
+        chrome.tabs.create({
+          url: 'chrome://extensions',
+        });
+      case 'history':
+        chrome.tabs.create({
+          url: 'chrome://history',
+        });
+      case 'bookmark_manager':
+        chrome.tabs.create({
+          url: 'chrome://bookmarks',
+        });
     }
   });
 });
